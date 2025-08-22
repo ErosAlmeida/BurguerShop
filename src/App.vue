8bd85ba5a289efe72134feb3c7cfcd2f088007ca
ErosAlmeida <eros.almeida.vilela@gmail.com>
2025-08-22 16:52:51 -0400

    Footer

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,44 @@
 <template>
-  <Navbar :logo="logo_src" :alt="app_name"/>
-  <router-view/>
-  <Footer/>
+  <div>
+    <Navbar :logo="logo_src" :alt="app_name" />
+    <div>
+      <router-view></router-view>
+    </div>
+    <Footer />
+  </div>
 </template>
-
 <script>
-import Navbar from './components/NavBar.vue'
-import Footer from './components/Footer.vue'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
 export default {
+  data: function() {
+    return {
+      logo_src: "/img/logo.png",
+      app_name: "Make Your Burger"
+    }
+  },
   components: {
     Navbar,
     Footer
-  },
-  data(){
-    return{
-      logo_src :'./img/logo.png',
-      app_name: 'Make Your Burguer'
-    }
   }
 }
 </script>
 
 <style>
-  *{
+  * {
     font-family: Helvetica;
-    padding: 0%;
-    margin: 0%;
+    padding: 0;
+    margin: 0;
     box-sizing: border-box;
   }
-  .main-container{
+
+  .main-container {
     margin: 50px;
-    min-height: 360px;
+    min-height: 250px;
   }
-  
-  h1{
+
+  h1 {
     text-align: center;
     font-size: 42px;
     margin-bottom: 30px;
--- src/components/BurgerForm.vue
+++ src/components/BurgerForm.vue
@@ -53,65 +53,52 @@ export default {
     }
   },
   methods: {
-  async getIngredientes() {
-    try {
-      const req = await fetch('http://localhost:3000/ingredientes');
-      const data = await req.json();
-      
-      this.paes = data.paes;
-      this.carnes = data.carnes;
-      this.opcionaisdata = data.opcionais;
-    } catch (err) {
-      console.error("Erro ao carregar ingredientes:", err);
-      
-      // Dados de fallback para desenvolvimento
-      this.paes = [{ id: 1, tipo: "Pão Francês (fallback)" }];
-      this.carnes = [{ id: 1, tipo: "Carne (fallback)" }];
-      this.opcionaisdata = [{ id: 1, tipo: "Queijo (fallback)" }];
-      
-      this.msg = "Erro ao carregar ingredientes. Usando dados locais.";
-      setTimeout(() => this.msg = "", 3000);
-    }
-  },
-  
-  async createBurger(e) {
-    e.preventDefault();
-    
-    try {
+    async getIngredientes() {
+      const req = await fetch('http://localhost:3000/ingredientes')
+      const data = await req.json()
+
+      this.paes = data.paes
+      this.carnes = data.carnes
+      this.opcionaisdata = data.opcionais
+    },
+    async createBurger(e) {
+
+      e.preventDefault()
+
       const data = {
         nome: this.nome,
         carne: this.carne,
         pao: this.pao,
         opcionais: Array.from(this.opcionais),
         status: "Solicitado"
-      };
+      }
+
+      const dataJson = JSON.stringify(data)    
 
       const req = await fetch("http://localhost:3000/burgers", {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(data)
+        headers: { "Content-Type" : "application/json" },
+        body: dataJson
       });
 
-      const res = await req.json();
-      console.log("Burger criado:", res);
+      const res = await req.json()
+
+      console.log(res)
+
+      this.msg = "Pedido realizado com sucesso!"
+
+      // clear message
+      setTimeout(() => this.msg = "", 3000)
+
+      // limpar campos
+      this.nome = ""
+      this.carne = ""
+      this.pao = ""
+      this.opcionais = []
       
-      this.msg = "Pedido realizado com sucesso!";
-      setTimeout(() => this.msg = "", 3000);
-      
-      // Reset do formulário
-      this.nome = "";
-      this.carne = "";
-      this.pao = "";
-      this.opcionais = [];
-      
-    } catch (err) {
-      console.error("Erro ao criar burger:", err);
-      this.msg = "Erro ao enviar pedido. Verifique o servidor.";
-      setTimeout(() => this.msg = "", 3000);
     }
-  }
-},
-  mounted() {
+  },
+  mounted () {
     this.getIngredientes()
   },
   components: {
