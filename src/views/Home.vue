3a18f2a53b3ed44bbf7cffef294439ae102fcc1d
ErosAlmeida <eros.almeida.vilela@gmail.com>
2025-08-22 16:27:47 -0400

    Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,40 +1,22 @@
 <template>
-  <div class="home">
-    <Banner/>
+  <div>
+    <Banner />
     <div class="main-container">
-      <h1>Monte seu burguer</h1>
-      <BurgerForm/>
+      <h1>Monte o seu burger:</h1>
+      <BurgerForm />
     </div>
   </div>
 </template>
 
 <script>
-import Banner from '../components/Banner.vue'
-import BurgerForm from '../components/BurguerForm.vue' // ← Mudei para BurguerForm.vue
+import Banner from '../components/Banner'
+import BurgerForm from '../components/BurgerForm'
 
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
     Banner,
     BurgerForm
   }
 }
 </script>
-
-<style scoped>
-.home {
-  padding: 20px;
-}
-
-.main-container {
-  max-width: 1200px;
-  margin: 0 auto;
-  text-align: center;
-}
-
-h1 {
-  color: #222;
-  margin-bottom: 30px;
-  font-size: 2.5rem;
-}
-</style>
