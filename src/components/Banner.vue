a4985456ca2967f82f364175ea22d5217fdaa2cb
ErosAlmeida <eros.almeida.vilela@gmail.com>
2025-08-22 17:18:18 -0400

    update

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -11,23 +11,22 @@
 </script>
 
 <style scoped>
+#main-banner {
+  
+  background-image: url('~@/../public/img/burger.jpg');
 
-  #main-banner {
-    background-image: url('/img/burger.jpg');
-    background-position: 0 -250px;
-    background-size: cover;
-    height: 500px;
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
-  }
-
-  #main-banner h1 {
-    color: #FFF;
-    text-align: center;
-    font-size: 60px;
-    background-color: #222;
-    padding: 20px 40px;
-  }
-
+  background-position: 0 -250px;
+  background-size: cover;
+  height: 500px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
+#main-banner h1 {
+  color: #FFF;
+  text-align: center;
+  font-size: 60px;
+  background-color: #222;
+  padding: 20px 40px;
+}
 </style>
